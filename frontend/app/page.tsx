d1ebc3517c397ef223e06759912a0eecf4d75f29
anthonykimani <kimaniantoni@gmail.com>
2024-04-19 13:44:29 +0300

    feat: Adds a Footer Section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,6 +7,7 @@ import HeroSection from '@/components/landing-page/HeroSection';
 import Navbar from '@/components/landing-page/Navbar';
 import RoadMap from '@/components/landing-page/RoadMap';
 import Team from '@/components/landing-page/Team';
+import Footer from '@/components/landing-page/Footer';
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         <Features />
         <RoadMap />
         <Team />
+        <Footer />
     </section>
   )
 }

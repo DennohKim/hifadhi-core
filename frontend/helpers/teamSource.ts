7b138b853baba1193e819cd04451c99117404731
Dennis Kimathi <kimathidennis38@gmail.com>
2024-10-09 16:35:21 +0300

    fix: fix image issue

--- frontend/helpers/teamSource.ts
+++ frontend/helpers/teamSource.ts
@@ -1,22 +1,22 @@
 export const teamSource = [
   {
-    img: "https://media.licdn.com/dms/image/D4D03AQH-RnUzaRWA-A/profile-displayphoto-shrink_800_800/0/1702815826317?e=1718841600&v=beta&t=jB5sFpUj0dhKN9nSsi2NY4CeV9UY0cK1cD_HSNz9ZYk",
+    img: "https://res.cloudinary.com/dqab6gg7d/image/upload/v1728480706/1702815826317_kblnkd.jpg",
     name: "Ibrahim Aziz",
     title: "Blockchain Engineer | Founder",
   },
   {
     img: "https://avatars.githubusercontent.com/u/63696529?v=4",
     name: "Dennis Kimathi",
-    title: "Blockchain Engineer | Product Manager",
+    title: "Product Manager | Frontend Developer",
   },
   {
-    img: "https://media.licdn.com/dms/image/D4D35AQEvYYGh4DaG-g/profile-framedphoto-shrink_800_800/0/1674787804159?e=1714129200&v=beta&t=BFhabW_HxDRUnQ6v-Vc1AMOdIuBhQ-KFBTgBuUWAamo",
+    img: "https://res.cloudinary.com/dqab6gg7d/image/upload/v1728479935/1622148223252_oyfxb2.jpg",
     name: "Ronex Ondimu",
     title: "Blockchain Engineer | Backend Engineer",
   },
   {
-    img: "https://media.licdn.com/dms/image/D4D03AQFtoNO_JdiUhA/profile-displayphoto-shrink_800_800/0/1712424388793?e=1718841600&v=beta&t=4vfKjGOPwdiqo998PvwP553AmQ9vFcb7FqqJy-XcJio",
-    name: "Dennis Kimathi",
-    title: "Frontend Engineer | UI Designer",
+    img: "https://res.cloudinary.com/dqab6gg7d/image/upload/v1728479935/1622148223252_oyfxb2.jpg",
+    name: "Anthony Kimani",
+    title: "Frontend Developer | UI Designer",
   },
 ];
